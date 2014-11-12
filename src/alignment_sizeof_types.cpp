a26ad6a1d53115b1ceb3a6c12200facf0db23294
Venki <ramana.sanaka@gmail.com>
2014-11-12 11:12:09 +0530

    Added class to check reference size

--- src/alignment_sizeof_types.cpp
+++ src/alignment_sizeof_types.cpp
@@ -20,6 +20,7 @@
 // SOFTWARE.
 
 #include <cstdint>
+#include <cstddef>
 
 #include <iostream>
 #include <string>
@@ -27,96 +28,108 @@
 using namespace std;
 
 #define POPULATE(t) \
-  print<t>(#t)
-
-#define offset_off(t, e) \
-  unsigned((char *)&(((t *)0)->e) - (char *)((t *)0))
+    print<t>(#t)
 
 template <typename type_t>
 void print(string s) {
-  cout << "type " << s
-       << " --- sizeof --- " << sizeof(type_t)
-       << " --- alignment --- " << __alignof__(type_t)
-       << endl;
+    cout << "type " << s
+        << " --- sizeof --- " << sizeof(type_t)
+        << " --- alignment --- " << __alignof__(type_t)
+        << endl;
 }
 
 class Sample {
-  public:
-    Sample() {}
-    //virtual
-    ~Sample() {}
+    public:
+        Sample() {}
+        //virtual
+        ~Sample() {}
 
-  private:
-    //
+    private:
+        //
 };
 
 struct structa_t {
-  char        c;
-  short int   s;
+    char        c;
+    short int   s;
 };
 
 struct structb_t {
-  short int   s;
-  char        c;
-  int         i;
+    short int   s;
+    char        c;
+    int         i;
 };
 
 struct structc_t {
-  char        c;
-  double      d;
-  int         s;
+    char        c;
+    double      d;
+    int         s;
 };
 
 struct structd_t {
-  double      d;
-  int         s;
-  char        c;
+    double      d;
+    int         s;
+    char        c;
 };
 
 class ref_size_t {
-public:
-    ref_size_t(int &y) : x(y) {}
+    public:
+        ref_size_t(int &y) : x(y) {}
 
-private:
-    int &x;
+    private:
+        int &x;
 };
 
+struct ref_offset_t {
+    ref_offset_t(int &y) : x(y) {}
+
+    int &x;
+    char c;
+};
+
+void test_01() {
+    off_t offset = offsetof(ref_offset_t, c);
+    std::cout << "c offset - " << offset << " bytes (space occupied by reference)" << std::endl;
+}
+
 int main(void) {
-  POPULATE(char);
-  POPULATE(short);
-  POPULATE(int);
-  POPULATE(long);
-  POPULATE(unsigned);
-  POPULATE(unsigned long);
-  POPULATE(double);
-  POPULATE(long double);
-  POPULATE(signed short);
-  POPULATE(unsigned short);
-  POPULATE(unsigned char);
-  POPULATE(void *);
-  POPULATE(long &);
-  POPULATE(short &);
-  POPULATE(short *);
-  POPULATE(int &);
-  POPULATE(const int);
+    POPULATE(char);
+    POPULATE(short);
+    POPULATE(int);
+    POPULATE(long);
+    POPULATE(unsigned);
+    POPULATE(unsigned long);
+    POPULATE(double);
+    POPULATE(long double);
+    POPULATE(signed short);
+    POPULATE(unsigned short);
+    POPULATE(unsigned char);
+    POPULATE(void *);
+    POPULATE(long &);
+    POPULATE(short &);
+    POPULATE(short *);
+    POPULATE(int &);
+    POPULATE(const int);
 
-  POPULATE(int8_t);
-  POPULATE(int16_t);
-  POPULATE(int32_t);
-  POPULATE(int64_t);
-  POPULATE(uint8_t);
-  POPULATE(uint16_t);
-  POPULATE(uint32_t);
-  POPULATE(uint64_t);
+    POPULATE(int8_t);
+    POPULATE(int16_t);
+    POPULATE(int32_t);
+    POPULATE(int64_t);
+    POPULATE(uint8_t);
+    POPULATE(uint16_t);
+    POPULATE(uint32_t);
+    POPULATE(uint64_t);
 
-  POPULATE(Sample);
-  POPULATE(structa_t);
-  POPULATE(structb_t);
-  POPULATE(structc_t);
-  POPULATE(structd_t); // This will surprise on i686 machines, with size 16 bytes at an alignment of 4 bytes
+    POPULATE(Sample);
+    POPULATE(structa_t);
+    POPULATE(structb_t);
+    POPULATE(structc_t);
+    POPULATE(structd_t); // This will surprise on i686 machines, with size 16 bytes at an alignment of 4 bytes
 
-  POPULATE(ref_size_t);
+    POPULATE(ref_size_t);
+    POPULATE(ref_offset_t);
 
-  return 0;
+    test_01();
+
+    return 0;
 }
 
